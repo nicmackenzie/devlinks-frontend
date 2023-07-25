42675803ddd1982127fd9d263f88f1b029b96041
nicmackenzie <nicmackenzie@live.com>
2023-07-25 08:23:48 +0300

    display side by side

--- src/components/ui/LoadingText.jsx
+++ src/components/ui/LoadingText.jsx
@@ -1,11 +1,19 @@
+import { styled } from 'styled-components';
 import Spinner from './Spinner';
+
+const StyledDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.25rem;
+`;
 
 function LoadingButtonText({ text }) {
   return (
-    <>
+    <StyledDiv>
       <Spinner size="small" />
       <span>{text}</span>
-    </>
+    </StyledDiv>
   );
 }
 
