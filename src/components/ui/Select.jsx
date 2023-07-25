0e2b28a9f7ceff3ef5ea74981aba92ed79adf826
nicmackenzie <nicmackenzie@live.com>
2023-07-25 16:34:32 +0300

    reusable select

---
+++ src/components/ui/Select.jsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+const StyledSelect = styled.select`
+  /* font-size: 0.875rem; */
+  font-size: 1.125rem;
+  /* padding: 0.5rem 0.75rem; */
+  padding: 0.375rem 0.75rem;
+  border: 1px solid
+    ${props =>
+      props.type === 'white'
+        ? 'var(--color-grey-100)'
+        : 'var(--color-grey-300)'};
+  border-radius: var(--border-radius-sm);
+  background-color: var(--color-grey-0);
+  font-weight: 500;
+  box-shadow: var(--shadow-sm);
+`;
+
+function Select({ options, value, onChange, type }) {
+  return (
+    <StyledSelect value={value} onChange={onChange} type={type}>
+      {options?.map(option => (
+        <option key={option.value} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </StyledSelect>
+  );
+}
+
+export default Select;
