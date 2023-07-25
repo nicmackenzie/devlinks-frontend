2f48434d6b6a0a8096e89d559cebd28075c80bbc
nicmackenzie <nicmackenzie@live.com>
2023-07-25 11:18:56 +0300

    Protected route component

---
+++ src/pages/ProtectedRoute.jsx
@@ -0,0 +1,36 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { styled } from 'styled-components';
+import Spinner from '../components/ui/Spinner';
+import { useAuth } from '../hooks/useAuth';
+
+const FullPage = styled.div`
+  height: 100dvh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+function ProtectedRoute({ children }) {
+  const { isLoading, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(
+    function () {
+      if (!isLoading && !isAuthenticated) navigate('/login', { replace: true });
+    },
+    [isLoading, isAuthenticated, navigate]
+  );
+
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+
+  if (isAuthenticated) return children;
+}
+
+export default ProtectedRoute;
