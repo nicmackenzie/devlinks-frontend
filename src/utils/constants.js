95c7f059bb3545592e810189a0d3940dd743e39a
nicmackenzie <nicmackenzie@live.com>
2023-07-25 16:34:03 +0300

    add stacks

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,48 @@
 export const API_URL = 'http://127.0.0.1:9292';
+
+export const STACKS = [
+  {
+    value: 1,
+    label: 'Python',
+  },
+  {
+    value: 2,
+    label: 'Javascript',
+  },
+  {
+    value: 3,
+    label: 'HTML/CSS',
+  },
+  {
+    value: 4,
+    label: 'React',
+  },
+  {
+    value: 5,
+    label: 'Vue',
+  },
+  {
+    value: 6,
+    label: 'Angular',
+  },
+  {
+    value: 7,
+    label: 'Ruby',
+  },
+  {
+    value: 8,
+    label: 'Sinatra',
+  },
+  {
+    value: 9,
+    label: 'Ruby on rails',
+  },
+  {
+    value: 10,
+    label: 'Flask',
+  },
+  {
+    value: 11,
+    label: 'Django',
+  },
+];
